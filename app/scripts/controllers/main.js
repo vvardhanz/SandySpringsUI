133b9a1fbcd08e8f088a42d1480eb6701c1bc993
vvardhan <kvvardhanz@gmail.com>
2016-01-02 23:39:10 -0500

    initial demo

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sandySpringsUiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sandySpringsUiApp
+ */
+angular.module('sandySpringsUiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
